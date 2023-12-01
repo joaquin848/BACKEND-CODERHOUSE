12f3c2de84d1a8a567da23101f42bfa06714dee3
joaquin848 <joaky848@gmail.com>
2023-12-01 13:19:51 -0300

    servidor en capas

--- src/db/config.DB.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import config from "../config.js";
-const URI = config.mongo_uri;
-
-mongoose
-  .connect(URI)
-  .then(() => console.log("🆗 Conectados a MongoDB"))
-  .catch((err) => console.log(err));
